f6770b768ce8b1bcf961739dd57922b2048e1eed
Konstantin Lugovskoy <tornadov.costya@yandex.ru>
2024-04-07 22:55:35 +0300

    Initial commit

---
+++ src/main/java/com/edu/tiktalk_backend/TiktalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.tiktalk_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktalkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/tiktalk_backend/TiktalkBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.edu.tiktalk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktalkBackendApplicationTests {
+
+
+
+}
